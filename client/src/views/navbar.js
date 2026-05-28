649c6ca48bab84095e6532fc2969d611db4c0dfe
Randall Naar <rnd.naar@gmail.com>
2026-05-28 13:35:47 -0400

    Updated to latest nav design.

--- client/src/views/navbar.js
+++ client/src/views/navbar.js
@@ -5,6 +5,13 @@ export default S =>
 
   <nav className="container nav-container">
       <a className="navbar-brand" href="."></a>
+        <div className="sub-nav font-h5">
+            <a href="." class={{ active: S.activeTab == 'dashBoard' }}>Dashboard</a>
+            <a href="blocks/recent" class={{ active: S.activeTab == 'recentBlocks' }}>Blocks</a>
+            <a href="tx/recent" class={{ active: S.activeTab == 'recentTxs' }}>Transactions</a>
+            { process.env.IS_ELEMENTS ? <a href="assets" class={{ active: S.activeTab == 'assets' }}>Assets<sup className="highlight"></sup></a> : "" }
+            <a href="/explorer-api" class={{ active: S.activeTab == 'apiLanding' }}>Explorer API</a>
+        </div>
       { menu(S) }
   </nav>
 
